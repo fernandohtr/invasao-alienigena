7c53e3ffe0ea2898e60e6855be33c28d4f390b41
Fernando Henrique <fernandohtr@gmail.com>
2019-04-26 20:58:08 -0300

    Adiciona fundo cinza claro na tela do jogo

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,9 @@ def run_game():
     screen = pygame.display.set_mode((1366, 768))  # default: (1200, 800)
     pygame.display.set_caption('Alien Invasion')
 
+    # Define a cor de fundo
+    bg_color = (230, 230, 230)
+
     # Inicia o laço principal do jogo
     while True:
 
@@ -16,6 +19,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # Redesenha a tela a cada pasagem pelo laço
+        screen.fill(bg_color)
 
         # Deixa a tela mais recente visível
         pygame.display.flip()
