0bb9c395f4d8430f1566b4a59c4b5467e8bb499e
Fernando Henrique <fernandohtr@gmail.com>
2019-04-27 08:38:45 -0300

    Adiciona game_functions

--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ class Settings():
     def __init__(self):
         """Inicializa as configuraççoes do jogo."""
         # Configurações da tela
-        self.screen_width = 1200
-        self.screen_height = 800
+        self.screen_width = 1366  # 1200
+        self.screen_height = 768  # 800
         self.bg_color = (230, 230, 230)
