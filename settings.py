1b8b3528732c61994db9844ccc315f7524362e75
Fernando Henrique <fernandohtr@gmail.com>
2019-04-29 07:44:48 -0300

    Adiciona movimento para direita

--- settings.py
+++ settings.py
@@ -6,5 +6,5 @@ class Settings():
         """Inicializa as configuraççoes do jogo."""
         # Configurações da tela
         self.screen_width = 1366  # 1200
-        self.screen_height = 768  # 800
+        self.screen_height = 716  # 800
         self.bg_color = (230, 230, 230)
