6ae3c804a0baf9a0447cf0b558b6a3dca3a341e8
Fernando Henrique <fernandohtr@gmail.com>
2019-05-01 21:20:06 -0300

    Adiciona update e draw_bullet em bullet

--- bullet.py
+++ bullet.py
@@ -28,3 +28,14 @@ class Bullet(Sprite):
 
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """Move o projétil para cima na tela."""
+        # Atualiza a posição decimal do projétil
+        self.y -= self.speed_factor
+        # Atualiza a posição de rect
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """Desenha o projétil na tela."""
+        pygame.draw.rect(self.creen, self.color, self.rect)
