b9764cf1cf9a83af8efd1d2c3ea9aa42ad65e3d4
Fernando Henrique <fernandohtr@gmail.com>
2019-07-12 19:13:05 -0300

    Garante a pontuação por cada alienígena atingido

--- game_functions.py
+++ game_functions.py
@@ -159,8 +159,9 @@ def check_bullet_alien_collisions(
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
 
     if collisions:
-        stats.score += ai_settings.alien_points
-        sb.prep_score()
+        for aliens in collisions.values():
+            stats.score += ai_settings.alien_points * len(aliens)
+            sb.prep_score()
 
     if len(aliens) == 0:
         # Destrói os projéteis existentes e cria uma nova frota
